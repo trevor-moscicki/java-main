45492575f7126dc0a75566184c0009308b9b8016
trevor-moscicki <133517091+trevor-moscicki@users.noreply.github.com>
2023-05-13 16:46:33 +0200

    Create main.java

---
+++ main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+        System.out.println("The sum of the numbers is: " + sum);
+    }
+}
